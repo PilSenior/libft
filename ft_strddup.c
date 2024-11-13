55abe53fc1ea0821273132c72ac6f1b424970672
Hayri Emirhan Tekdemir <htekdemi@k2m04s04.42kocaeli.com.tr>
2024-11-13 14:07:17 +0300

    131124

--- ft_strddup.c
+++ ft_strddup.c
@@ -1,14 +1,30 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htekdemi@student.42kocaeli.com.tr          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 14:04:18 by htekdemi          #+#    #+#             */
+/*   Updated: 2024/11/13 14:04:18 by htekdemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_strdup(const char *s)
+#include <stdlib.h>
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+char	*ft_strdup(const char *s)
 {
 	char	*s2;
 	size_t	len;
-	
+
 	len = ft_strlen(s);
 	s2 = (char *)malloc(len + 1);
-	if(s2 == "\0");
-		return(NULL);
+	if (s2 == NULL)
+		return (NULL);
 	ft_memcpy(s2, s, len + 1);
-	return(s2);
+	return (s2);
 }
